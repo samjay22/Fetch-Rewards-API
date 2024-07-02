30bdb2fe8b49ff08c3c0928b1e15a23bfeddbfc2
Samuel Taylor <samjaytaylor22@gmail.com>
2024-07-01 22:42:23 -0500

    Optimizations

--- Backend/Services/DatabaseService.go
+++ Backend/Services/DatabaseService.go
@@ -9,14 +9,11 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-// databaseService implements the Interfaces.DatabaseService interface
 type databaseService struct {
 	db     *sql.DB
 	logger *zerolog.Logger
-	args   *ScopedConstructorArgs
 }
 
-// NewDatabaseServiceArgs contains the arguments required to create a new DatabaseService
 type NewDatabaseServiceArgs struct {
 	Logger           *zerolog.Logger
 	Cfg              *Structs.Config
@@ -24,12 +21,7 @@ type NewDatabaseServiceArgs struct {
 	ConnectionString string
 }
 
-// ScopedConstructorArgs contains the arguments required to create a new scoped DatabaseService
-type ScopedConstructorArgs struct {
-	Args *NewDatabaseServiceArgs
-}
-
-// NewDatabaseService creates a new instance of DatabaseService
+// NewDatabaseService creates a new instance of DatabaseService.
 func NewDatabaseService(args *NewDatabaseServiceArgs) Interfaces.DatabaseService {
 	db, err := sql.Open("sqlite", args.ConnectionString)
 	if err != nil {
@@ -42,65 +34,48 @@ func NewDatabaseService(args *NewDatabaseServiceArgs) Interfaces.DatabaseService
 	return &databaseService{
 		db:     db,
 		logger: args.Logger,
-		args:   &ScopedConstructorArgs{Args: args},
 	}
 }
 
-func (d *databaseService) withNewConnection(action func(db *sql.DB) (interface{}, error)) (interface{}, error) {
-	db, err := sql.Open("sqlite", d.args.Args.ConnectionString)
+func (d *databaseService) GetEntityByFilterRule(ctx context.Context, filterRule func(connection interface{}) (interface{}, error)) (interface{}, error) {
+	obj, err := filterRule(d.db)
 	if err != nil {
-		d.logger.Fatal().Err(err).Msg("Failed to connect to database")
+		d.logger.Error().Err(err).Msg("Failed to execute filter rule")
 		return nil, err
 	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			d.logger.Error().Err(err).Msg("Failed to close database connection")
-		}
-	}()
-	return action(db)
+	return obj, nil
 }
 
-// GetEntityByFilterRule retrieves an entity based on the provided filter rule
-func (d *databaseService) GetEntityByFilterRule(ctx context.Context, filterRule func(connection interface{}) (interface{}, error)) (interface{}, error) {
-	return d.withNewConnection(func(db *sql.DB) (interface{}, error) {
-		obj, err := filterRule(db)
-		if err != nil {
-			d.logger.Error().Err(err).Msg("Failed to execute filter rule")
-			return nil, err
-		}
-		return obj, nil
-	})
-}
-
-// UpdateEntityByFilterRule updates an entity based on the provided filter rule
 func (d *databaseService) UpdateEntityByFilterRule(ctx context.Context, filterRule func(connection interface{}) (bool, error)) (bool, error) {
-	result, err := d.withNewConnection(func(db *sql.DB) (interface{}, error) {
-		return filterRule(db)
-	})
+	result, err := filterRule(d.db)
 	if err != nil {
+		d.logger.Error().Err(err).Msg("Failed to execute update rule")
 		return false, err
 	}
-	return result.(bool), nil
+	return result, nil
 }
 
-// DeleteEntityByFilterRule deletes an entity based on the provided filter rule
 func (d *databaseService) DeleteEntityByFilterRule(ctx context.Context, filterRule func(connection interface{}) (bool, error)) (bool, error) {
-	result, err := d.withNewConnection(func(db *sql.DB) (interface{}, error) {
-		return filterRule(db)
-	})
+	result, err := filterRule(d.db)
 	if err != nil {
+		d.logger.Error().Err(err).Msg("Failed to execute delete rule")
 		return false, err
 	}
-	return result.(bool), nil
+	return result, nil
 }
 
-// AddEntity adds a new entity using the provided delegate function
 func (d *databaseService) AddEntity(ctx context.Context, delegate func(connection interface{}) error) error {
-	_, err := d.withNewConnection(func(db *sql.DB) (interface{}, error) {
-		if err := delegate(db); err != nil {
-			return nil, err
-		}
-		return nil, nil
-	})
-	return err
+	if err := delegate(d.db); err != nil {
+		d.logger.Error().Err(err).Msg("Failed to add entity")
+		return err
+	}
+	return nil
+}
+
+func (d *databaseService) Close() error {
+	if err := d.db.Close(); err != nil {
+		d.logger.Error().Err(err).Msg("Failed to close database connection")
+		return err
+	}
+	return nil
 }
